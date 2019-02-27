d6863226b3b09c3cdc75a00f1e7669421f35e1d4
alex <alex_is_ok@yahoo.com>
2019-02-27 21:06:52 +0200

    ui_modded

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
     if current_user
       graph = Koala::Facebook::API.new(ENV["MY_TOKEN"])
       f = graph.get_connections("me", "friends")
+      f == [] ? f = 'Something went wrong' : f
     else
       f = 'Ups !.. U forgot to log in...'
     end
