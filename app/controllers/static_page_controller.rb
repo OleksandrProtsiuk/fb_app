06dbb890259e821831d5ca62bf4d07ebe78a9489
alex <alex_is_ok@yahoo.com>
2019-02-27 18:34:34 +0200

    bootstrap

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,5 +1,12 @@
 class StaticPageController < ApplicationController
+  http_basic_authenticate_with name: "1", password: "1", except: [:home, :about]
+
   def index
+    if current_user
+      redirect home_url
+    else
+      'ok'
+    end
   end
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_page/index'
-  get 'static_page/home'
-  get 'static_page/about'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  root 'templates#index', as: 'root'
+  root 'static_page#index', as: 'root'
+
+  get 'static_page/index'
+  get 'static_page/home', as: 'home'
+  get 'static_page/about', as: 'about'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :templates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
