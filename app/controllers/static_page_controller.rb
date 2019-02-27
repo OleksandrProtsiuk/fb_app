eb0d168a4ce6a28fc3bbc1bc3c220646f687240e
alex <alex_is_ok@yahoo.com>
2019-02-27 20:04:25 +0200

    fix_in_single_page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,20 +2,9 @@ class StaticPageController < ApplicationController
   http_basic_authenticate_with name: "1", password: "1", except: [:home, :about]
 
   def index
-    if current_user
-      redirect_to home_path
-    else
-      'ok'
-    end
   end
 
   def home
-    #if current_user
-     # @graph = Koala::Facebook::API.new(access_token)
-      #@friends = @graph.get_connections("me", "friends")
-    #else
-     # @friends = 'Ups !.. U forgot to log in...'
-    #end
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def friends
+    if current_user
+      graph = Koala::Facebook::API.new(ENV["MY_TOKEN"])
+      f = graph.get_connections("me", "friends")
+    else
+      f = 'Ups !.. U forgot to log in...'
+    end
+    f
+  end
+
 end
