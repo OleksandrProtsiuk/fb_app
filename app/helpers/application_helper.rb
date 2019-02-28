8413803b10193bb2648fc1693d01b5f5bfeece89
alex <alex_is_ok@yahoo.com>
2019-02-28 14:45:17 +0200

    mechanize test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,10 @@ module ApplicationHelper
     f
   end
 
+  def send_message
+    a = Mechanize.new
+    a.get('https://www.facebook.com/messages/t/100014458980881')
+    [a.current_page.title, a.current_page.uri, a.current_page.form.name, a.current_page.links]
+  end
+
 end
